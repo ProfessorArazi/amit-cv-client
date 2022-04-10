08f653530b35301dd56700bf6f38f25966838daa
Amit <amitarazi12345@gmail.com>
2022-04-10 17:24:29 +0300

    changed to sass

--- src/Componenets/Projects/Projects.jsx
+++ src/Componenets/Projects/Projects.jsx
@@ -56,7 +56,7 @@ const Projects = () => {
       title: "Sudoku",
       text: (
         <p>
-          This is a sudoku game which I created his client side with partner.
+          This is a sudoku game which I created his client side with a partner.
           <br /> I added a server side so users can sign up and try to get into
           the top ten results
         </p>
