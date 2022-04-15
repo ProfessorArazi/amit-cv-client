ea87cfb4c02cfc1f1a76dfdf13b0edc8d325d968
Amit <amitarazi12345@gmail.com>
2022-04-15 18:03:34 +0300

    added periods to projects desc

--- src/Componenets/Projects/Projects.jsx
+++ src/Componenets/Projects/Projects.jsx
@@ -33,13 +33,14 @@ const Projects = () => {
       title: "Super Compare",
       text: (
         <p>
-          This is a comparison site that compares three well-known markets{" "}
+          This is a comparison site that compares three well-known markets.
           <br />
           I created a scraper to get all the latest prices from each market so
-          the prices will always be up to date <br />
+          the prices will always be up to date. <br />
           Thanks to that I give you the ability to choose the cheapest market
-          according to what you are interested in purchasing <br />
-          The comparison takes into account the discounts that the markets offer
+          according to what you are interested in purchasing. <br />
+          The comparison takes into account the discounts that the markets
+          offer.
         </p>
       ),
       link: "https://super-compare.vercel.app",
