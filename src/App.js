b2913d22907c2ad2f682c0f0e2f1aaca008d2ac3
Amit Arazi <amitarazi123456@gmail.com>
2023-03-27 11:59:50 +0300

    navbar active link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -17,7 +17,14 @@ function App() {
     Aos.init({ duration: 1500 });
   }, []);
 
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+
   const [expanded, setExpanded] = useState(false);
+  const [activeSection, setActiveSection] = useState("#home");
 
   const closeToggleHandler = () => {
     setExpanded(false);
@@ -26,6 +33,48 @@ function App() {
   const toggleHandler = () => {
     setExpanded(expanded ? false : "expanded");
   };
+
+  const onScroll = (e) => {
+    const heights = [
+      {
+        id: "#home",
+        position: homeRef.current?.offsetTop,
+        height: homeRef.current?.clientHeight,
+      },
+      {
+        id: "#about",
+        position: aboutRef.current?.offsetTop,
+        height: aboutRef.current?.clientHeight,
+      },
+      {
+        id: "#skills",
+        position: skillsRef.current?.offsetTop,
+        height: skillsRef.current?.clientHeight,
+      },
+      {
+        id: "#projects",
+        position: projectsRef.current?.offsetTop,
+        height: projectsRef.current?.clientHeight,
+      },
+      {
+        id: "#contact",
+        position: contactRef.current?.offsetTop,
+        height: contactRef.current?.clientHeight,
+      },
+    ];
+    const windowHeight = Math.ceil(window.pageYOffset);
+    const avtive = heights.find(
+      (height) =>
+        windowHeight >= height.position &&
+        windowHeight < height.position + height.height
+    );
+    setActiveSection(avtive.id);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
   return (
     <>
@@ -36,8 +85,8 @@ function App() {
             onClick={toggleHandler}
           />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link active href="#home" onClick={closeToggleHandler}>
+            <Nav activeKey={activeSection} className="me-auto">
+              <Nav.Link href="#home" onClick={closeToggleHandler}>
                 Home
               </Nav.Link>
               <Nav.Link href="#about" onClick={closeToggleHandler}>
@@ -57,15 +106,26 @@ function App() {
         </Container>
       </Navbar>
       <main>
-        <section onClick={closeToggleHandler} id="home" className="home">
+        <section
+          ref={homeRef}
+          onClick={closeToggleHandler}
+          id="home"
+          className="home"
+        >
           <Home />
         </section>
 
-        <section onClick={closeToggleHandler} id="about" className="about">
+        <section
+          ref={aboutRef}
+          onClick={closeToggleHandler}
+          id="about"
+          className="about"
+        >
           <About />
         </section>
         <div id="skills"></div>
         <section
+          ref={skillsRef}
           data-aos="fade-up"
           data-aos-once
           onClick={closeToggleHandler}
@@ -76,6 +136,7 @@ function App() {
         <div id="projects"></div>
 
         <section
+          ref={projectsRef}
           data-aos="fade-up"
           data-aos-once
           onClick={closeToggleHandler}
@@ -87,6 +148,7 @@ function App() {
         <div id="contact"></div>
 
         <section
+          ref={contactRef}
           data-aos="fade-up"
           data-aos-once
           onClick={closeToggleHandler}
